a2bbc6ba2cba762f1f464c80c28b03d89e84f94a
100thCoin <onehundredthcoin@gmail.com>
2025-04-06 21:44:58 -0400

    Addressed some roommate-feedback, 15 minutes until submission hour ends.

---
+++ Assets/Scripts/MysteryCaveArrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryCaveArrow : MonoBehaviour {
+
+	public SpriteRenderer SR;
+	public float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Global.Dataholder.CountGems () == 2 && !Global.Dataholder.NecromooncerDefeated && !Global.Dataholder.CamMov.Boss1Camera) {
+			timer += Time.deltaTime;
+			if (timer > 0.5f) {
+				SR.enabled = true;
+			} else {
+				SR.enabled = false;
+			}
+			if (timer > 1) {
+				timer -= 1;
+			}
+		}
+
+		
+	}
+}
